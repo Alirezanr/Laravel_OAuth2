9c4e254129f3dd4946a0c62462ce59fc90765653
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-09 18:38:41 +0330

    Create AuthController.php . Add Authentication api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::namespace('Api')->group(function ()
+{
+    Route::prefix('auth')->group(function ()
+    {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/signup', [AuthController::class, 'signup']);
+    });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['middleware' => 'auth:api'], function ()
+    {
+        Route::get('hi', [AuthController::class, 'hello']);
+    });
+
+
 });
