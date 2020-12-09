f6f3800c306b55124de9c754816ca4b98b93b687
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-09 20:07:39 +0330

    finish signup functionality in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,18 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    function login()
+    function login(Request $request)
     {
-        return "Hello from AuthController";
+        $request->validate(['email'    => 'required|string|email',
+                            'password' => 'required|string']);
     }
-    function signup()
+
+    function signup(Request $request)
     {
-        return "Hello from AuthController";
+        $request->validate(['name'     => 'required|string',
+                            'email'    => 'required|string|email|unique:users',
+                            //confirmed:we need to pass tow passwords and both should match
+                            'password' => 'required|string|confirmed']);
+
+
+        $user = new User(['name'     => $request->name,
+                          'email'    => $request->email,
+                          'password' => bcrypt($request->password)]);
+
+        $user->save();
+
+        return response()->json(['message' => 'user registered successfully.'], 201);
     }
+
     function hello()
     {
         return "Hello from AuthController";
