d4907f3b0236a47cf19c864b4cfaf71bc432ed3e
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-09 18:34:22 +0330

    Install passport and configure it.

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function login()
+    {
+        return "Hello from AuthController";
+    }
+    function signup()
+    {
+        return "Hello from AuthController";
+    }
+    function hello()
+    {
+        return "Hello from AuthController";
+    }
+}
